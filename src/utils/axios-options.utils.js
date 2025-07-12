8d9fdbe8f2490e42b64381222e62ea6fdb519645
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 03:46:47 -0500

    create utils opcions for axios

---
+++ src/utils/axios-options.utils.js
@@ -0,0 +1,8 @@
+export const options = {
+	method: "GET",
+	headers: {
+		accept: "application/json",
+		Authorization:
+			"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmM2IxZDVkOWY4NmY5NjA3NWM2ZTM1NzNlM2I1MjIzYiIsIm5iZiI6MTc1MjI3NjczNS42NjIwMDAyLCJzdWIiOiI2ODcxOWVmZmRjNjFkM2JmOTY1NDFiODYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.J4zLF7c6JPkA_5EwZxwpl6uQsdHbqx80VEXJuSVuw70",
+	},
+};
