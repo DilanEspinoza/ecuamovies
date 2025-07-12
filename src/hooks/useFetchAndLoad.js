2fd7f453f5877ff75689f615d910b8bdb9bf0b86
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 03:51:29 -0500

    create the custom hook useFetchAndLoad

---
+++ src/hooks/useFetchAndLoad.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+export const useFetchAndLoad = () => {
+	const [loading, setLoading] = useState(false);
+	let controller;
+
+	const callEndPoint = async (axiosCall) => {
+		if (axiosCall.controller) controller = axiosCall.controler;
+		setLoading(true);
+
+		let result = {};
+		try {
+			result = await axiosCall.call;
+		} catch (error) {
+			console.error(error);
+		}
+		// handle error
+		setLoading(false);
+		return result;
+	};
+
+	const cancelEndPoint = () => {
+		setLoading(false);
+		controller && controller.abort();
+	};
+
+	useEffect(() => {
+		return () => {
+			cancelEndPoint();
+		};
+	}, []);
+
+	return { loading, callEndPoint };
+};
