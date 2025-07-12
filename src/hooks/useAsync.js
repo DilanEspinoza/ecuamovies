218df89ed17c71f982f3a56e53feb23e1255a9fa
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 03:51:08 -0500

    create the custom hook useAsync

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+export const useAsync = (
+	asyncFn,
+	successFunction,
+	returnFuntion,
+	dependencies
+) => {
+	useEffect(() => {
+		let isActive = true;
+		asyncFn().then((result) => {
+			if (isActive) successFunction(result.data);
+		});
+		return () => {
+			returnFuntion && returnFuntion();
+			isActive = false;
+		};
+	}, dependencies);
+};
