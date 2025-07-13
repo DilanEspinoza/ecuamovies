9acc8d78f13eb24636060bf2f7dcbfc59b1e6485
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 23:19:09 -0500

    add the personal config for each function

--- src/services/public.services.js
+++ src/services/public.services.js
@@ -7,13 +7,14 @@ const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const getMoviesByCategory = (category = "popular") => {
 	const controller = loadAbort();
+	const config = {
+		...options, // headers y method
+		signal: controller.signal,
+	};
 	return {
 		call: axios.get(
 			`${VITE_BASE_URL}/movie/${category}?language=en-US&page=1`,
-			options,
-			{
-				signal: controller.signal,
-			}
+			config
 		),
 		controller,
 	};
@@ -21,25 +22,44 @@ export const getMoviesByCategory = (category = "popular") => {
 
 export const getSingleMovie = (movie_id) => {
 	const controller = loadAbort();
+	const config = {
+		...options, // headers y method
+		signal: controller.signal,
+	};
 	return {
 		call: axios.request(
 			`${VITE_BASE_URL}/movie/${movie_id}?language=en-US`,
-			options,
-			{
-				signal: controller.signal,
-			}
+			config
 		),
 		controller,
 	};
 };
 
+export const getSearchMovies = (query) => {
+	const controller = loadAbort();
+	const config = {
+		...options, // headers y method
+		signal: controller.signal,
+		params: { query },
+	};
+	return {
+		call: axios.get(
+			`${VITE_BASE_URL}/search/movie?include_adult=false&language=en-US&page=1`,
+			config
+		),
+		controller,
+	};
+};
+
+/* 
 export const getSimilarMovies = (movie_id) => {
 	const controller = loadAbort();
 	return {
 		call: axios.get(
-			`${VITE_BASE_URL}//movie/${movie_id}/similar?language=en-US&page=1`,
+			`${VITE_BASE_URL}/movie/${movie_id}/similar?language=en-US&page=1`,
 			{ signal: controller.signal }
 		),
 		controller,
 	};
 };
+ */
