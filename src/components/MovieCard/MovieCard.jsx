c422bd9b78f16f460670a3a19f84dda6f639bcd5
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 23:36:30 -0500

    fix: resolve bug of image undefined

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router"
+import placeholder from "/img-placeholder.jpg";
 
 export const MovieCard = ({ movie_id, poster_path }) => {
+
+
     return (
-        <Link to={`/movie/${movie_id}`}>
-            <article className="w-56  p-4 ">
-                <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="" />
+        <Link to={`/movie/${movie_id}`} className=" ">
+            <article className="w-56  p-4  ">
+                {poster_path ?
+                    <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={`Movie poster ${movie_id}`} className="hover:rounded-2xl" />
+                    : <img src={placeholder} alt={`Movie poster ${movie_id}`} className="w-56 h-72  object-fill hover:rounded-2xl" />}
+
             </article>
         </Link>
     )
