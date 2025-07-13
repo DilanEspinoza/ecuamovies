62f668e7c843369a271030fa9dc3882454da392e
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 23:11:15 -0500

    feat: create new page

---
+++ src/pages/SearchMoviePage/SearchMoviePage.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router";
+import { getSearchMovies } from "../../services/public.services";
+import { useFetchAndLoad } from "../../hooks/useFetchAndLoad";
+import { useAsync } from "../../hooks/useAsync";
+import { Header } from "../../components/Header/Header";
+import { MovieCard } from "../../components/MovieCard/MovieCard";
+import { MovieCartSkeleton } from "../../components/MovieCartSkeleton/MovieCartSkeleton";
+
+export default function SearchMoviePage() {
+
+
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get('search');
+
+
+    const [movies, setMovies] = useState([])
+
+    const { loading, callEndPoint } = useFetchAndLoad()
+
+    const getApiData = async () => await callEndPoint(getSearchMovies(query))
+    const adaptMovies = (data) => setMovies(data.results)
+
+    useAsync(getApiData, adaptMovies, () => { }, [query])
+
+
+    return (
+        <div className="w-3/4 mx-auto">
+
+            <Header />
+
+            <main className="w-4/5 mx-auto flex flex-wrap gap-10 justify-between">
+
+                {loading ? (<>
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                    <MovieCartSkeleton />
+                </>) : movies.length <= 0 ? <p className="font-semibold text-lg">There are no movies to show</p> : movies.map((movie) => (
+                    <MovieCard key={movie.id} movie_id={movie.id} poster_path={movie.poster_path} />
+                ))
+                }
+            </main>
+        </div>
+    )
+}
