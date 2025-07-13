56d661f973ba1cfaf73bef826814cfcbda71c493
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 23:13:14 -0500

    create the component movie card skeleton

---
+++ src/components/MovieCartSkeleton/MovieCartSkeleton.jsx
@@ -0,0 +1,6 @@
+export const MovieCartSkeleton = () => {
+    return (
+        <div className="w-44 md:w-52 lg:w-56 p-2 animate-pulse">
+            <div className="w-full h-72 bg-gray-200 rounded-lg"></div>
+        </div>)
+}
