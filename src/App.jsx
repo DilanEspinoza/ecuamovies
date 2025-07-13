4e978d7a658f62ee5988ebfca4d82609006d1a73
Dilan Espinoza <dilan.developer22@gmail.com>
2025-07-12 23:20:15 -0500

    add new path of movie and context search

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router"
 import MoviePage from "./pages/MoviePage/MoviePage"
 import Home from "./pages/Home/Home"
 import { createContext, useState } from "react"
+import SearchMoviePage from "./pages/SearchMoviePage/SearchMoviePage"
 
+const SearchContext = createContext()
 const MovieContext = createContext()
 
 function App() {
 
+  const [searchContext, setSearchContext] = useState("")
   const [categoryMovies, setCategoryMovies] = useState("popular")
 
 
@@ -15,17 +18,20 @@ function App() {
 
 
   return (
-    <MovieContext value={{ categoryMovies, setCategoryMovies }}>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/movie/:movie_id" element={<MoviePage />} />
-        </Routes>
-      </BrowserRouter>
-    </MovieContext>
+    <SearchContext.Provider value={{ searchContext, setSearchContext }}>
+      <MovieContext value={{ categoryMovies, setCategoryMovies }}>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/movie/:movie_id" element={<MoviePage />} />
+            <Route path="/movie" element={<SearchMoviePage />} />
+          </Routes>
+        </BrowserRouter>
+      </MovieContext>
+    </SearchContext.Provider>
   )
 }
 
-export { MovieContext }
+export { SearchContext, MovieContext }
 
 export default App
